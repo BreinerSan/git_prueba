48c77d6e156fbb9dcfee18c19e8afc97c4795a12
BREINER <breiner.sv@gmail.com>
2018-02-13 10:29:50 -0500

    Se agrego un nuevo texto al index

--- html/index.php
+++ html/index.php
@@ -10,5 +10,6 @@
 	<span> <?php echo $variable ?> </span>	 
 	<h4>Este cambio es relacionado con la rama Test</h4><br>
 	<h5>SOLO prueba de la rama TEst</h5>
+	<p>Agregamos un texto nuevo en la rama text para nueva prueba de texto</p>
 </body>
 </html>
