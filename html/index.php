3e80fe8ce0c2f7931a5e8ff2906cc7227a62ccdf
BREINER <breiner.sv@gmail.com>
2018-02-13 11:58:24 -0500

    Modificamos el index

--- html/index.php
+++ html/index.php
@@ -3,13 +3,19 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Primera vista</title>
+	<style>
+		h1{
+			font-family: Monospace;
+			font-size: 40px;
+			color: #21afde;
+		}
+	</style>
 </head>
 <body>
 	
 	<h1>ESTAMOS EN LA PRIMERA VISTA</h1>
 	<span> <?php echo $variable ?> </span>	 
-	<h4>Este cambio es relacionado con la rama Test</h4><br>
-	<h5>SOLO prueba de la rama TEst</h5>
-	<p>Agregamos un texto nuevo en la rama text para nueva prueba de texto</p>
+	<h4>Este cambio es Proyecto subido a GitHub solo como prueba de vinculacion de git con github</h4><br>
+	
 </body>
 </html>
